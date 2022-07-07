1ffdc81a386f867d98e6079db54d367c21950725
HyunMin Cho <100783766+Chynmn@users.noreply.github.com>
2022-07-08 00:18:08 +0900

    Sum all integers between two integers

--- 25.c
+++ 25.c
@@ -3,7 +3,27 @@
 
 int main(void)
 {
-    int inum1, inum2;
+    int inum1, inum2, total = 0;
+
+    printf("Input two num to sum all integers between two integers\n");
+    printf("(ex: 1 50)\n");
+    scanf("%d %d", &inum1, &inum2);
+
+    if(inum1 < inum2){
+        while(inum1 <= inum2){
+            total += inum1; 
+            inum1++;
+        }
+        printf("The total is %d\n", total);
+    }
+    else if(inum2 < inum1){
+        while(inum2 <= inum1){
+            total += inum2;
+            inum2++;
+        }
+        printf("The total is %d\n", total);
+    }
+    
 
     return 0;
 }
