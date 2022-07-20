133d6c818549470b7dfd6b3cebf3f757ad2fe32b
HyunMin Cho <100783766+Chynmn@users.noreply.github.com>
2022-07-20 20:53:22 +0900

    decimal to binary number

--- 0531_1.c
+++ 0531_1.c
@@ -5,7 +5,7 @@ int main(void)
 {
     int num, cnt = 0, i = 0;
     int ejinsu[100] = { 0 };
-    printf("10진수를 입력하여 2진수로 변환하는 프로그램\n숫자 입력 : ");
+    printf("Program to convert decimal number to binary number\nInput decimal : ");
     scanf("%d", &num);
 
     while(1){
@@ -20,6 +20,8 @@ int main(void)
     for(i = cnt - 1; i >= 0; i--){
         printf("%d", ejinsu[i]);
     }
-printf("\n");
+
+    printf("\n");
+    
     return 0;
 }
